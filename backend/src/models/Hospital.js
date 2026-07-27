ad0f31af322c67b8ac98c5786ee760025dd8adb6
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-07-28 00:27:23 +0200

    .

--- backend/src/models/Hospital.js
+++ backend/src/models/Hospital.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const hospitalSchema = new mongoose.Schema({
   name: { type: String, required: true },
   type: { type: String, enum: ['hospital', 'clinic', 'laboratory'], required: true },
-  location: { type: String, required: }
+  location: { type: String, required: true},
 });
 
 module.exports = mongoose.model('Hospital', hospitalSchema);
